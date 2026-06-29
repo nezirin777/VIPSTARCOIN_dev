c3ba798e90dc8e36b36d5c77a44daca4e647a7f8
nezirin <nezinezi54321@gmail.com>
2026-06-30 04:33:56 +0900

    fix: Correct tool help name in bitcoin-util.cpp, and update staking threadName in miner.cpp
    - bitcoin-util.cpp 内のヘルプ表示テキスト qtum-util を vipstarcoin-util へ修正。
    - miner.cpp 内のステーキング用スレッド名を qtumstake から vipsstake へ変更。

--- src/miner.cpp
+++ src/miner.cpp
@@ -1439,7 +1439,7 @@ public:
 
     {
         // Make this thread recognisable as the mining thread
-        std::string threadName = "qtumstake";
+        std::string threadName = "vipsstake";
         if(pwallet && pwallet->GetName() != "")
         {
             threadName = threadName + "-" + pwallet->GetName();
